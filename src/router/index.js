4415a80e46625e41447a425ecc18fe18fac368be
huanmingchang <huanming1988@gmail.com>
2022-04-04 10:08:40 +0800

    feat: add search component and styles

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,24 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Search from '../views/Search.vue'
 
-Vue.use(VueRouter);
+Vue.use(VueRouter)
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
-    component: Home,
+    path: '/',
+    name: 'root',
+    redirect: '/search',
   },
-];
+  {
+    path: '/search',
+    name: 'Search',
+    component: Search,
+  },
+]
 
 const router = new VueRouter({
   routes,
-});
+})
 
-export default router;
+export default router
---
+++ src/views/Search.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="search p-8 mt-16">
+    <h1 class="search-title text-h3">Search</h1>
+    <v-text-field
+      class="input mx-auto my-10"
+      label="Type item code"
+      :rules="rules"
+      hide-details="auto"
+    ></v-text-field>
+    <div class="my-2">
+      <v-btn color="primary" dark> search </v-btn>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.search {
+  height: 100vh;
+  width: 100vw;
+  text-align: center;
+}
+
+.input {
+  width: 50%;
+}
+</style>
